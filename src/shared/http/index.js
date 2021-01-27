1b112f19ca93ceec4557e19b05f685734e6c4d9a
Marcio Robson <robsonmrsp@gmail.com>
2021-01-26 23:57:41 -0300

    Add a module to wrap the http requests, abstracting the fetch api used

---
+++ src/shared/http/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import btoa from 'btoa';
+
+const BASE_URL = 'https://aersoftwares.com/vendemais/';
+
+export default class HttpRequest {
+  // constructor(url) {
+  //   if (!url) {
+  //     throw new Error('Need to define a sufix to endpoint!');
+  //   }
+  //   this.url = url;
+  // }
+
+  createAuthHeaders = () => {
+    const headers = {};
+    headers['Authorization'] = `Basic ${btoa('flordamodaadm:123456')}`;
+    headers['Content-Type'] = 'application/json';
+
+    console.log(`Basic ${btoa('XXXXX:123456')}`);
+    return headers;
+  };
+
+  get = async (url) => {
+    const response = await axios.get(`${BASE_URL}${url}/`, { headers: this.createAuthHeaders() });
+    return response.data;
+  };
+
+  getPage = async (params) => {
+    const response = await axios.get(BASE_URL + this.url, { headers: this.createAuthHeaders(), params });
+    return response.data;
+  };
+
+  save = (object) => {
+    const response = fetch(BASE_URL + this.url, {
+      method: 'post',
+      headers: this.createAuthHeaders(),
+      body: JSON.stringify(object),
+    });
+    return response;
+  };
+
+  delete = () => {};
+}
