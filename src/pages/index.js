806f061a040585dad2dae1ab072b5ef998397c78
Marcio Robson <robsonmrsp@gmail.com>
2021-01-27 23:58:55 -0300

    Add categories to globalstate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,29 @@
 import PageLayout from '@/components/layout/PageLayout';
+import { EcommerceContext } from '@/shared/context';
 import HttpRequest from '@/shared/http';
+import { useContext, useEffect } from 'react';
 
-const Index = ({ data }) => (
-  <PageLayout>
-    <div>Index data {data.itens.length} </div>
-  </PageLayout>
-);
+const Index = ({ categories }) => {
+  const { state, update } = useContext(EcommerceContext);
+
+  update({ categories });
+
+  useEffect(() => {
+    console.log(state);
+  });
+  return (
+    <PageLayout>
+      <div>Index data {categories.length} </div>
+    </PageLayout>
+  );
+};
 
 export const getServerSideProps = async (ctx) => {
   const httpRequest = new HttpRequest();
-  const data = await httpRequest.get('rs/crud/productCategorys');
-  console.log(data);
+  const categories = await httpRequest.get('rs/crud/productCategorys/basic');
   return {
     props: {
-      data,
+      categories,
     },
   };
 };
