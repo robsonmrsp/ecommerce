c798b140c7035c12f90e716727f233c6a675cf48
Marcio Robson <robsonmrsp@gmail.com>
2021-01-28 23:38:07 -0300

    add header to cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -4,6 +4,32 @@ const Cart = () => {
   console.log('cart');
   return (
     <PageLayout>
+      <div className="page-title-overlap bg-dark pt-4">
+        <div className="container d-lg-flex justify-content-between py-2 py-lg-3">
+          <div className="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb breadcrumb-light flex-lg-nowrap justify-content-center justify-content-lg-start">
+                <li className="breadcrumb-item">
+                  <a className="text-nowrap" href="index.html">
+                    <i className="czi-home" />
+                    Home
+                  </a>
+                </li>
+                <li className="breadcrumb-item text-nowrap">
+                  <a href="shop-grid-ls.html">Shop</a>
+                </li>
+                <li className="breadcrumb-item text-nowrap active" aria-current="page">
+                  Cart
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <div className="order-lg-1 pr-lg-4 text-center text-lg-left">
+            <h1 className="h3 text-light mb-0">Your cart</h1>
+          </div>
+        </div>
+      </div>
+
       <div className="container pb-5 mb-2 mb-md-4">
         <div className="row">
           {/* List of items */}
@@ -284,4 +310,3 @@ const Cart = () => {
 };
 
 export default Cart;
-
