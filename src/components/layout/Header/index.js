e8dfffa8bf3331b572b10d0a63932eea960b1e67
Marcio Robson <robsonmrsp@gmail.com>
2021-02-21 23:24:36 -0300

    Fix position fixed on header

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -12,14 +12,10 @@ export const Header = () => {
   console.log(state);
   return (
     // checar o scrol para adicionar a classe navbar-stuck
-    <div className="bg-light">
+    <div className="bg-light fixed-top">
       <div className="navbar navbar-expand-lg navbar-light">
         <Container>
-          <Link
-            className="navbar-brand d-none d-sm-block mr-3 flex-shrink-0"
-            href="/home"
-            style={{ minWidth: '7rem' }}
-          >
+          <Link className="navbar-brand d-none d-sm-block mr-3 flex-shrink-0" href="/home" style={{ minWidth: '7rem' }}>
             <Image width={142} src="img/logo-dark.png" alt="Vendemais" />
           </Link>
           <Link className="navbar-brand d-sm-none mr-2" href="index.html" style={{ minWidth: '4.625rem' }}>
